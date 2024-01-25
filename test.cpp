53581e1f974f2097f7150699c062cac8f14aa7d1
TinTranT <22110076@student.hcmute.edu.vn>
2024-01-25 15:31:21 +0700

    them bien a

--- test.cpp
+++ test.cpp
@@ -1,12 +1,13 @@
-//branch Nguyen Mai Huy Hoang day
-//hello everyone
-//test lan 2
-// fheofhfpf
-// daskfhaskdjf
-// UTE student
+// branch Nguyen Mai Huy Hoang day
+// hello everyone
+// test lan 2
+//  fheofhfpf
+//  daskfhaskdjf
+//  UTE student
 #include <iostream>
 using namespace std;
 int main()
 {
+    int a = 5;
     return 0;
 }
